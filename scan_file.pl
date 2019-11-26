35c8e7bd6857094dea2cf8a0cb6c2bcd826cc250
sahlite <spiritmongerster.gmail.com>
2019-11-26 22:36:11 +0800

    增加scan_file.pl注释部分

--- scan_file.pl
+++ scan_file.pl
@@ -1,28 +1,27 @@
-#path, suffix, reg
-#my ($path, $suffix, $rp) = @ARGV;
-my ($path, $suffix) = @ARGV;
-@suffix = split " ", $suffix; # 记得把字符串，转化为数组
+my ($path, $suffix) = @ARGV;  # 执行时需要输入 1-要找的文件路径 2-要找的文件类型（扩展名）
+@suffix = split " ", $suffix; # 将扩展名字符串转化为数组，每个类型使用空格分割
 
+# 扫描制定路径下的文件，按照是路径还是文件分别处理
 sub scan_file
 {
-  my ($path) = @_;
-  my @files = glob($path);
-  foreach my $file (@files)
+  my ($path) = @_;             # 定义路径变量，保存输入信息
+  my @files = glob($path);     # 提取路径下的所有文件，生成文件名称的数组
+  foreach my $file (@files)    # 遍历文件数组，依次操作
   {
-    if(-d $file)
-    { # 文件递归下去
-      scan_file("$file/*");
-    }
-    elsif(-f $file)
+    if(-d $file)               # 路径则递归处理
     {
-      foreach my $su (@suffix)
+      scan_file("$file/*");    # 
+    }
+    elsif(-f $file)            # 文件则根据文件类型筛选
+    {
+      foreach my $su (@suffix) # 遍历文件扩展名
       {
-        if ($file =~ /$su$/)
+        if ($file =~ /$su$/)   # 检查文件名结尾是否匹配
         {
-          print $file."\n";
+          print $file."\n";    # 输出符合的文件名称
         }
       }
     }
   }
 }
-scan_file($path);
+scan_file($path);              # 处理输入的文件路径
