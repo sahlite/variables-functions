6b8c5e2c9a977094a7239631136ae3bf66efa8ae
sahlite <spiritmongerster.gmail.com>
2019-11-28 22:39:31 +0800

    修订bash脚本，调用perl错误

--- myscript.sh
+++ myscript.sh
@@ -3,7 +3,7 @@ echo "start to scan files"
 perl scan_file.pl "./*" ".c" > files_list.txt
 echo "scan files finished"
 echo "start to pair var- file"
-perl variabl-file.pl "./files_list.txt" "variable_lists.txt" > variable-file.txt
+perl variable-file.pl "./files_list.txt" "variable_lists.txt" > variable-file.txt
 echo "pair var-file  finished"
 echo "start to pair var-func"
 perl var-func.pl "variable-file.txt" "file-function.txt"
